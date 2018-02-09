b8135440c9147d0137fddca5756cc53b753af3f7
hualuomoli <liubaoquan0831@163.com>
2018-02-09 13:34:21 +0800

    增加了日志的调试，优化了调试的流程

--- app/src/main/java/com/github/hualuomoli/android/MainActivity.java
+++ app/src/main/java/com/github/hualuomoli/android/MainActivity.java
@@ -1,44 +1,33 @@
 package com.github.hualuomoli.android;
 
+import android.content.Intent;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.github.hualuomoli.android.logger.LoggerActivity;
 import com.github.hualuomoli.logger.Level;
 import com.github.hualuomoli.logger.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG1 = "MainActivity1";
-    private static final String TAG2= "MainActivity2";
-    private static final String TAG3 = "MainActivity3";
+    private Button testLoggerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 未设置级别，默认关闭
-        Logger.verbose(TAG1, "this is verbose message.");
-        Logger.debug(TAG1, "this is debug message.");
-        Logger.info(TAG1, "this is info message.");
-        Logger.warn(TAG1, "this is warn message.");
-        Logger.error(TAG1, "this is errro message.");
+        testLoggerButton = findViewById(R.id.testLogger);
+        testLoggerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoggerActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        // 设置级别为warn
-        Logger.setLevel(Level.WARN);
-        Logger.verbose(TAG2, "this is verbose message.");
-        Logger.debug(TAG2, "this is debug message.");
-        Logger.info(TAG2, "this is info message.");
-        Logger.warn(TAG2, "this is warn message.");
-        Logger.error(TAG2, "this is errro message.");
-
-        // 设置级别为ALL
-        Logger.setLevel(Level.ALL);
-        Logger.verbose(TAG3, "this is verbose message.");
-        Logger.debug(TAG3, "this is debug message.");
-        Logger.info(TAG3, "this is info message.");
-        Logger.warn(TAG3, "this is warn message.");
-        Logger.error(TAG3, "this is errro message.");
-
+        // end
     }
 }
